d6e81921e653c0a7f699b9cb4af27193d2a9bbca
Barney Fabian <bafa@tutanota.de>
2018-03-01 19:07:44 +0000

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,13 @@
 class Project
-
-  attr_accessor :title, :backers
-
+  attr_accessor :backers, :title
+  
   def initialize(title)
     @title = title
     @backers = []
   end
-
+  
   def add_backer(backer)
     @backers << backer
     backer.backed_projects << self
   end
-
 end
